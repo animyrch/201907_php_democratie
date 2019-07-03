454e21d8cd803ae0566c6032f54faffb69b21bf0
Emir OZCELIK <e.ozcelik@lodecom.fr>
2019-07-03 15:32:14 +0200

    removed debug for successful db connection. Integrated Bulma as CSS framework

--- app/production/inc/db-connect.inc.php
+++ app/production/inc/db-connect.inc.php
@@ -3,7 +3,6 @@ require_once __DIR__."/../../config/env.php";
 
 try{
     $db = new PDO("mysql:host=$_DBHOST; dbname=$_DBNAME;", "$_DBUSER", "$_DBMDP");
-    echo "connection successfull";
 }catch(PDOException $e){
     echo "Erreur!: " . $e->getMessage() . "<br/>";
     die();
